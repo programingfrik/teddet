ce4f33b9395139a329e60ad0946c29235aefcf48
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-04-16 14:26:18 -0400

    En esta versión se muestra el menú como esperaba. Pero todavía falta.

--- src/teddetui/teddetui.py
+++ src/teddetui/teddetui.py
@@ -5,21 +5,38 @@ import tkinter as tki
 from tkinter import ttk
 #import libteddet
 
-class TeddetUI(tki.Frame):
-    def __init__(self, master = None):
-        tki.Frame.__init__(self, master)
-        top = tki.Frame(master)
-        top.master.title("teddetui")
-        top.option_add("*tearOff", False)
-        win = tki.Toplevel(top)
-        menu_prin = tki.Menu(win)
-        win["menu"] = menu_prin
+class TeddetUI():
+    def __init__(self, # master = None
+                 ):
+        # tki.Frame.__init__(self, master)
+        # top = self
+        # top.master.title("teddetui")
+        root = tki.Tk()
+        #top = tki.Toplevel(root)
+
+        root.option_add("*tearOff", False)
+
+        # win = self.master
+        menu_prin = tki.Menu(root)
+        root["menu"] = menu_prin
         menu_file = tki.Menu(menu_prin)
         menu_prin.add_cascade(menu = menu_file, label = "File")
+        # print(win.configure())
+
+        # root = tki.Tk(className = "TeddetUI")
+        # frm = ttk.Frame(root)
+        # frm.grid()
+        # self.menubar = tki.Menu(root)
+        # self.top = top = tki.window.ListedTopLevel(root, menu = self.menubar)
+
+        print(f"root.winfo_name: " + root.winfo_name())
+        print(f"root.__class__: {root.__class__}")
+        print(f"dir(root): {dir(root)}")
+        root.mainloop()
 
 def main():
     app = TeddetUI()
-    app.mainloop()
+    # app.mainloop()
 
 if __name__ == "__main__":
     main()
