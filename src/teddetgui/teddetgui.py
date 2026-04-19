65c7fb582cf0cb8592b0e2dcde4a2bbd7a4dd150
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-04-19 12:54:26 -0400

    Comenzando a poner las funciones que se ejecutan en cada comando del menú.

--- src/teddetgui/teddetgui.py
+++ src/teddetgui/teddetgui.py
@@ -3,61 +3,39 @@
 
 import tkinter as tki
 from tkinter import ttk
+from tkinter import messagebox
 #import libteddet
 
-class TeddetUI(#Frame
-):
-    def __init__(self, # master = None
-                 ):
-        # tki.Frame.__init__(self, master)
+class TeddetGUI():
 
-        # top = self
+    def __init__(self):
         self.root = tki.Tk()
-        #top = tki.Toplevel(root)
-        # win = self.master
-
         self.root.title("TEDDET")
-
         self.build_menu()
-
-        # print(win.configure())
-        # root = tki.Tk(className = "TeddetUI")
-        # frm = ttk.Frame(root)
-        # frm.grid()
-        # self.menubar = tki.Menu(root)
-        # self.top = top = tki.window.ListedTopLevel(root, menu = self.menubar)
-
-        # print(f"root.winfo_name: " + root.winfo_name())
-        # print(f"root.__class__: {root.__class__}")
-        # print(f"dir(root): {dir(root)}")
-
         self.root.mainloop()
 
     def build_menu(self):
         self.root.option_add("*tearOff", False)
 
-        self.menubar = tki.Menu(self.root)
-        self.root["menu"] = self.menubar
+        self.menub = tki.Menu(self.root)
+        self.root["menu"] = self.menub
 
-        menu_file = tki.Menu(self.menubar)
-        self.menubar.add_cascade(menu = menu_file, label = "File")
+        menu_file = tki.Menu(self.menub)
+        self.menub.add_cascade(menu = menu_file, label = "File")
         menu_file.add_command(label = "New", command = self.com_new)
         menu_file.add_command(label = "Open", command = self.com_open)
         menu_file.add_command(label = "Reload", command = self.com_open)
         menu_file.add_command(label = "Save", command = self.com_save)
         menu_file.add_command(label = "Save As", command = self.com_save)
-
-        menu_edit = tki.Menu(self.menubar)
-        self.menubar.add_cascade(menu = menu_edit, label = "Edit")
+        menu_edit = tki.Menu(self.menub)
+        self.menub.add_cascade(menu = menu_edit, label = "Edit")
         menu_edit.add_command(label = "Copy", command = self.com_copy)
         menu_edit.add_command(label = "Paste", command = self.com_paste)
-
-        menu_form = tki.Menu(self.menubar)
-        self.menubar.add_cascade(menu = menu_form, label = "Format")
-        menu_form.add_command(label = "choose Format", command = self.com_choose_form)
-
-        menu_help = tki.Menu(self.menubar)
-        self.menubar.add_cascade(menu = menu_help, label = "Help")
+        menu_frmt = tki.Menu(self.menub)
+        self.menub.add_cascade(menu = menu_frmt, label = "Format")
+        menu_frmt.add_command(label = "Choose Format", command = self.com_choose_form)
+        menu_help = tki.Menu(self.menub)
+        self.menub.add_cascade(menu = menu_help, label = "Help")
         menu_help.add_command(label = "Manual", command = self.com_manual)
         menu_help.add_command(label = "About", command = self.com_about)
 
@@ -83,12 +61,14 @@ class TeddetUI(#Frame
         pass
 
     def com_about(self):
+        messagebox.showinfo(message = "Un pollo cruzó la calle", title = "Algo pasó", icon = messagebox.INFO)
+
         pass
 
 
+
 def main():
-    app = TeddetUI()
-    # app.mainloop()
+    app = TeddetGUI()
 
 if __name__ == "__main__":
     main()
