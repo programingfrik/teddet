8582adaa02f504fd3ec1c4584f821a8de53483ad
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-05-01 09:29:23 -0400

    Aparentemente prepare.py está completo ... por ahora.

--- prepare.py
+++ prepare.py
@@ -25,33 +25,38 @@ import pathlib
 
 projectdir = None
 cwdrel = None
+pyinterpath = None
+inter = None
 
 def com_execute(subject):
-    global projectdir, cwdrel
-
-    command = ""
+    global projectdir, cwdrel, pyinterpath, inter
+    print(f"com_execute {subject}")
+    path = ""
     args = []
-    
     if subject == "teddetgui":
-        command = "cd d:\\pmercader\\proyectos\\teddet\\src && \"c:\\Program Files\\Python314\\python.exe\" -m teddetgui"
-        args = []
+        os.chdir(projectdir.joinpath("src"))
+        path = pyinterpath.joinpath(inter)
+        args += [path, "-m", "teddetgui"]
     elif subject == "libteddet_tests":
-        command = "haxe -main TestBasics --interp -L utest"
-        args = []
+        os.chdir(projectdir.joinpath("src", "libteddet_tests"))
+        path = get_bin_path("haxe").joinpath("haxe")
+        args = [path, "-main", "TestBasics", "--interp", "-L", "utest"]
     else:
-        print(f"Error: {subject} is unknown")
+        print(f"Error: don't know how to execute {subject}")
         return
-
-    os.execv()
+    os.execv(path, args)
 
 def com_build(subject):
     global projectdir, cwdrel
+    print(f"com_build {subject}")
     if subject == "libteddet":
-        command = ""
-        args = []
-        print("Not implemented yet")
+        os.chdir(projectdir)
+        path = get_bin_path("haxe").joinpath("haxe")
+        args = [path, pathlib.Path("src", "libteddet", "build.hxml")]
     else:
         print(f"Error: don't know how to build {subject}")
+        return
+    os.execv(path, args)
 
 def com_pack():
     print("Not implemented yet")
@@ -66,14 +71,22 @@ def infere_action():
     else:
         com_execute("teddetgui")
 
-def get_projectdir():
-    global projectdir, cwdrel
+def get_bin_path(bin):
+    return [pathlib.Path(p, bin) for p in os.get_exec_path()
+            if pathlib.Path(p, bin).exists()][0].parent
+
+def get_paths():
+    global projectdir, cwdrel, pyinterpath, inter
     parts = pathlib.Path(os.getcwd()).parts
     projectdir = pathlib.Path(*parts[:parts.index("teddet") + 1])
     cwdrel = pathlib.Path(*parts[parts.index("teddet") + 1:])
+    pyinterpath = pathlib.Path(sys.orig_argv[0]).parent
+    inter = pathlib.Path(sys.orig_argv[0]).name
+    if not pyinterpath.is_absolute():
+        pyinterpath = get_bin_path(inter)
 
 def main():
-    get_projectdir()
+    get_paths()
     cliparser = argparse.ArgumentParser()
     cliparser.add_argument("command", help = "Sub-command that yout want to execute", nargs = "?")
     cliparser.add_argument("subject", help = "Subject of the command", nargs = "?")
