29d116c581389fc37f3c45f99eabc194798e4ff7
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-08-01 12:59:21 -0400

    Now we got the unrepeated colors of the haxe logo.

--- src/teddetgui/about.py
+++ src/teddetgui/about.py
@@ -12,16 +12,39 @@ class about_teddet(Toplevel):
     def create_content(self):
         cvlogoTD = tki.Canvas(self, height = 200, width = 200)
         cvlogoTD.grid(row = 0, column = 1)
-        self.dibujar_logoTD(cvlogoTD, 10, "#55F", "#55A")
+        self.draw_logoTD(cvlogoTD, 5, "#77F", "#55A")
 
-    def dibujar_T(self, canvas, scale, x, y, fg, bg):
+        cvlogoPF = tki.Canvas(self, height = 200, width = 200)
+        cvlogoPF.grid(row = 1, column = 1)
+        self.draw_logoPF(cvlogoPF, 5, "", "", "#550")
+
+        cvlogoHX = tki.Canvas(self, height = 200, width = 200)
+        cvlogoHX.grid(row = 2, column = 1)
+        colors = ["#FFF"]
+        colors += [""] * 13
+        colors[7] = colors[1] = "#F68712"
+        colors[2] = "#F47216"
+        colors[13] = colors[3] = "#FBC707"
+        colors[6] = colors[4] = "#F1471D"
+        colors[9] = colors[5] = "#F25C19"
+        colors[8] = "#F89C0E"
+        colors[12] = colors[10] = "#FFF200"
+        colors[11] = "#FAB20B"
+        self.draw_logoHX(cvlogoHX, 5, colors)
+
+        cvlogoPY = tki.Canvas(self, height = 200, width = 200)
+        cvlogoPY.grid(row = 2, column = 2)
+        self.draw_logoPY(cvlogoPY, 5, "#005", "", "")
+
+
+    def draw_T(self, canvas, scale, x, y, fg, bg):
         canvas.create_rectangle(x, y, (scale * 12) + x, (scale * 4) + y,
                                 fill = fg, outline = fg)
         canvas.create_rectangle((scale * 4) + x, (scale * 4) + y,
                                 (scale * 8) + x, (scale * 12) + y,
                                 fill = fg, outline = fg)
 
-    def dibujar_D(self, canvas, scale, x, y, fg, bg):
+    def draw_D(self, canvas, scale, x, y, fg, bg):
         canvas.create_rectangle(x, y, (scale * 4) + x, (scale * 12) + y,
                                 fill = fg, outline = fg)
         canvas.create_arc((scale * 4) + x, y,
@@ -39,11 +62,57 @@ class about_teddet(Toplevel):
                                 (scale * 6) + x, (scale * 8) + y,
                                 fill = bg, outline = bg)
 
-    def dibujar_logoTD(self, canvas, scale, fg, bg):
+    def draw_E(self, canvas, scale, x, y, fg, bg):
+        canvas.create_rectangle(x, y, (scale * 6) + x, (scale * 7) + y,
+                                fill = fg, outline = fg)
+        canvas.create_rectangle((scale * 1) + x, (scale * 1) + y,
+                                (scale * 5) + x, (scale * 6) + y,
+                                fill = bg, outline = bg)
+        canvas.create_rectangle((scale * 2) + x, (scale * 2) + y,
+                                (scale * 5) + x, (scale * 3) + y,
+                                fill = fg, outline = fg)
+        canvas.create_rectangle((scale * 2) + x, (scale * 4) + y,
+                                (scale * 5) + x, (scale * 5) + y,
+                                fill = fg, outline = fg)
+
+
+    def draw_logoTD(self, canvas, scale, fg, bg):
         canvas.create_rectangle(0, 0, (scale * 27), (scale * 27),
-                                fill = bg, width = 0)
-        self.dibujar_T(canvas, scale, scale, scale, fg, bg)
-        self.dibujar_D(canvas, scale, scale * 14, scale, fg, bg)
-        self.dibujar_D(canvas, scale, scale, scale * 14, fg, bg)
-        self.dibujar_T(canvas, scale, scale * 14, scale * 14, fg, bg)
+                                fill = bg, outline = bg)
+        self.draw_T(canvas, scale, scale, scale, fg, bg)
+        self.draw_D(canvas, scale, scale * 14, scale, fg, bg)
+        self.draw_E(canvas, scale, scale * 11, scale * 6, fg, bg)
+        self.draw_D(canvas, scale, scale, scale * 14, fg, bg)
+        self.draw_T(canvas, scale, scale * 14, scale * 14, fg, bg)
+        self.draw_E(canvas, scale, scale * 11, scale * 19, fg, bg)
+
+    def draw_logoPF(self, canvas, scale, cwhi, cbla, cora):
+        canvas.create_rectangle(0, 0, 200, 200, fill = cora)
+
+
+    def draw_triHX(self, canvas, scale, a, b, c, color):
+        pass
+
+    def draw_wingHX(self, canvas, scale, a, b, colors):
+        ha = 15
+        hb = 5
+        c = (0, 0)
+        self.draw_triHX(canvas, scale, a, b, c, colors[0])
+
+    def draw_logoHX(self, canvas, scale, colors):
+        canvas.create_rectangle(0, 0, 200, 200, fill = colors[0])
+        a = (100, 50)
+        b = (150, 100)
+        c = (100, 150)
+        d = (50, 100)
+        canvas.create_polygon(*list(a + b + c + d),
+                              fill = colors[1], width = 0)
+        self.draw_wingHX(canvas, scale, a, b, colors[2:5])
+        self.draw_wingHX(canvas, scale, b, c, colors[5:8])
+        self.draw_wingHX(canvas, scale, c, d, colors[8:11])
+        self.draw_wingHX(canvas, scale, d, a, colors[11:14])
+
+    def draw_logoPY(self, canvas, scale, cblue, cyell, cwhi):
+        canvas.create_rectangle(0, 0, 200, 200, fill = cblue)
+        pass
 
