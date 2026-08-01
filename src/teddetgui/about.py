838129a015505621e586711def015243cbb6b9b5
Pablo Mercader Alcántara <programingfrik@gmail.com>
2026-08-01 14:34:06 -0400

    Refactoring to make more clear the drawing of TEDDET logo.

--- src/teddetgui/about.py
+++ src/teddetgui/about.py
@@ -36,55 +36,47 @@ class about_teddet(Toplevel):
         cvlogoPY.grid(row = 2, column = 2)
         self.draw_logoPY(cvlogoPY, 5, "#005", "", "")
 
+    def rectTS(self, canvas, scale, t, a, b, fill, outline):
+        canvas.create_rectangle((scale * a[0]) + t[0], (scale * a[1]) + t[1],
+                               (scale * b[0]) + t[0], (scale * b[1]) + t[1],
+                               fill = fill, outline = outline)
 
-    def draw_T(self, canvas, scale, x, y, fg, bg):
-        canvas.create_rectangle(x, y, (scale * 12) + x, (scale * 4) + y,
-                                fill = fg, outline = fg)
-        canvas.create_rectangle((scale * 4) + x, (scale * 4) + y,
-                                (scale * 8) + x, (scale * 12) + y,
-                                fill = fg, outline = fg)
+    def arcTS(self, canvas, scale, t, a, b, fill, outline, start, extent, style):
+        canvas.create_arc((scale * a[0]) + t[0], (scale * a[1]) + t[1],
+                          (scale * b[0]) + t[0], (scale * b[1]) + t[1],
+                          fill = fill, outline = outline, start = start,
+                          extent = extent, style = style)
 
-    def draw_D(self, canvas, scale, x, y, fg, bg):
-        canvas.create_rectangle(x, y, (scale * 4) + x, (scale * 12) + y,
-                                fill = fg, outline = fg)
-        canvas.create_arc((scale * 4) + x, y,
-                          (scale * 12) + x, (scale * 12) + y,
-                          fill = fg, outline = fg, start = -90,
-                          extent = 180, style = tki.CHORD)
-        canvas.create_rectangle((scale * 4) + x, y,
-                                (scale * 8) + x, (scale * 12) + y,
-                                fill = fg, outline = fg)
-        canvas.create_arc((scale * 4) + x, (scale * 4) + y,
-                          (scale * 8) + x, (scale * 8) + y,
-                          fill = bg, outline = bg, start = -90,
-                          extent = 180, style = tki.CHORD)
-        canvas.create_rectangle((scale * 4) + x, (scale * 4) + y,
-                                (scale * 6) + x, (scale * 8) + y,
-                                fill = bg, outline = bg)
+    def draw_T(self, canvas, scale, t, fg, bg):
+        t = (scale * t[0], scale * t[1])
+        self.rectTS(canvas, scale, t, (0, 0), (12, 4), fg, fg)
+        self.rectTS(canvas, scale, t, (4, 4), (8, 12), fg, fg)
 
-    def draw_E(self, canvas, scale, x, y, fg, bg):
-        canvas.create_rectangle(x, y, (scale * 6) + x, (scale * 7) + y,
-                                fill = fg, outline = fg)
-        canvas.create_rectangle((scale * 1) + x, (scale * 1) + y,
-                                (scale * 5) + x, (scale * 6) + y,
-                                fill = bg, outline = bg)
-        canvas.create_rectangle((scale * 2) + x, (scale * 2) + y,
-                                (scale * 5) + x, (scale * 3) + y,
-                                fill = fg, outline = fg)
-        canvas.create_rectangle((scale * 2) + x, (scale * 4) + y,
-                                (scale * 5) + x, (scale * 5) + y,
-                                fill = fg, outline = fg)
+    def draw_D(self, canvas, scale, t, fg, bg):
+        t = (scale * t[0], scale * t[1])
+        self.rectTS(canvas, scale, t, (0, 0), (4, 12), fg, fg)
+        self.arcTS(canvas, scale, t, (4, 0), (12, 12), fg, fg,
+                   -90, 180, tki.CHORD)
+        self.rectTS(canvas, scale, t, (4, 0), (8, 12), fg, fg)
+        self.arcTS(canvas, scale, t, (4, 4), (8, 8), bg, bg,
+                   -90, 180, tki.CHORD)
+        self.rectTS(canvas, scale, t, (4, 4), (6, 8), bg, bg)
 
+    def draw_E(self, canvas, scale, t, fg, bg):
+        t = (scale * t[0], scale * t[1])
+        self.rectTS(canvas, scale, t, (0, 0), (6, 7), fg, fg)
+        self.rectTS(canvas, scale, t, (1, 1), (5, 6), bg, bg)
+        self.rectTS(canvas, scale, t, (2, 2), (5, 3), fg, fg)
+        self.rectTS(canvas, scale, t, (2, 4), (5, 5), fg, fg)
 
     def draw_logoTD(self, canvas, scale, fg, bg):
-        canvas.create_rectangle(0, 0, (scale * 27), (scale * 27),
-                                fill = bg, outline = bg)
-        self.draw_T(canvas, scale, scale, scale, fg, bg)
-        self.draw_D(canvas, scale, scale * 14, scale, fg, bg)
-        self.draw_E(canvas, scale, scale * 11, scale * 6, fg, bg)
-        self.draw_D(canvas, scale, scale, scale * 14, fg, bg)
-        self.draw_T(canvas, scale, scale * 14, scale * 14, fg, bg)
-        self.draw_E(canvas, scale, scale * 11, scale * 19, fg, bg)
+        self.rectTS(canvas, scale, (0, 0), (0, 0), (27, 27), bg, bg)
+        self.draw_T(canvas, scale, (1, 1), fg, bg)
+        self.draw_D(canvas, scale, (14, 1), fg, bg)
+        self.draw_E(canvas, scale, (11, 6), fg, bg)
+        self.draw_D(canvas, scale, (1, 14), fg, bg)
+        self.draw_T(canvas, scale, (14, 14), fg, bg)
+        self.draw_E(canvas, scale, (11, 19), fg, bg)
 
     def draw_logoPF(self, canvas, scale, cwhi, cbla, cora):
         canvas.create_rectangle(0, 0, 200, 200, fill = cora)
